70251fad38364168157cca02348273ba3e138e86
Salva Aragón Reyes <salva.aragonreyes@gmail.com>
2021-02-21 17:38:57 +0100

    feat: add persistor to App component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,20 @@
 import Routes from '@Components/Routes';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import configureStore from '@Store/configStore';
+import { PersistGate } from 'redux-persist/integration/react';
 
 const App: React.FC = () => {
+  const { persistor, store } = configureStore();
   return (
-    <div>
-      <Router>
-        <Routes />
-      </Router>
-    </div>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Routes />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 };
 
