0f62013f5040cfd1723c7bbde3a41af021fc236a
Salva Aragón Reyes <salva.aragonreyes@gmail.com>
2021-02-21 17:42:06 +0100

    feat: create component for load camera and make photos

---
+++ src/components/CameraStatus/Camera/styled.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+type CameraContainerType = {
+  height: string;
+  width: string;
+  border: string;
+};
+
+export const CameraContainer = styled.div.attrs(
+  ({ height, width, border }: CameraContainerType) => ({
+    height,
+    width,
+    border,
+  }),
+)`
+  height: ${({ height }) => height};
+  width: ${({ width }) => width};
+  border: ${({ border }) => border};
+  border-radius: 2rem;
+  text-align: center;
+`;
+
+export const CustomVideo = styled.video`
+  height: 100%;
+`;
+
+export const CustomCanvas = styled.canvas`
+  display: none;
+`;
